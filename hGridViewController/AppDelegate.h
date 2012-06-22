f5b919ffa3185474405d1c0c3cec3f15552e58e7
Hymn <Hymn@pongsils-MacBook-Pro.local>
2012-06-22 19:25:16 +0700

    Initial Commit

---
+++ hGridViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hGridViewController
+//
+//  Created by pongsil vachirajongkol on 6/22/55 BE.
+//  Copyright (c) 2555 pongsil.v@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hGridViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hGridViewController
+//
+//  Created by pongsil vachirajongkol on 6/22/55 BE.
+//  Copyright (c) 2555 pongsil.v@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
